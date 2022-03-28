73c546b5bb02e3038d9a1659f17853c74659a6f8
Marianna <marianka101@gmail.com>
2022-03-28 14:01:36 +0200

    feat: p.1 - member model added

--- tinder/tinder_app/models.py
+++ tinder/tinder_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    firstName = models.CharField(max_length = 100, blank = True, null = True)
+    lastName = models.CharField(max_length = 100, blank = True, null = True)
+    email = models.EmailField()
+    image = models.ImageField(upload_to='pictures/%Y/%m/%d', blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.lastName + ' ' + self.firstName)
+
